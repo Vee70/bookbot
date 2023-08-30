4e3a5e336cfadcc5541b2a81a8a767149ff655e6
Vee70 <daravichett@gmail.com>
2023-08-31 00:18:53 +0700

    create a function that return a dict of letters and the number of time each one appears in the book

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import string
 file_path = 'books/frankenstein.txt'
 
 with open(file_path) as f:
     file_contents = f.read()
     words = file_contents.split()
-    print(len(words))
+
+    # create a dict to store letters and number times each on appears
+    letter_counts = {letter: 0 for letter in string.ascii_lowercase}
+
+    for letter in file_contents.lower():
+        if letter in letter_counts:
+            letter_counts[letter] += 1
+    
+    print(letter_counts)
