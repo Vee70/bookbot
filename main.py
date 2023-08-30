f33979275ec4dd5cbaeb4a2b52bae768f528857f
Vee70 <daravichett@gmail.com>
2023-08-31 00:34:36 +0700

    convert letter_counts to a list of tuple (letter, counts) then sort the list by counts

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ file_path = 'books/frankenstein.txt'
 
 with open(file_path) as f:
     file_contents = f.read()
-    words = file_contents.split()
 
-    letter_counts = get_letter_counts(file_contents)
+total_words = len(file_contents.split())
+
+letter_counts = get_letter_counts(file_contents)
+letter_counts = list(letter_counts.items())
+letter_counts.sort(key=lambda x: x[1], reverse=True)
     
-    print(letter_counts)
